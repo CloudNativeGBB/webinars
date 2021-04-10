0bdd54c57ba4c106efe23ad38816b34818c19e15
Ray Kao <ray@peopleandcode.com>
2021-04-10 00:12:47 -0400

    fixing networking settings

--- 2021-04-14-configure-your-aks-cluster-with-confidence/terraform/module-aks.tf
+++ 2021-04-14-configure-your-aks-cluster-with-confidence/terraform/module-aks.tf
@@ -24,36 +24,20 @@ module "aks" {
 		network_plugin			= "azure"
 		network_policy			= "calico"
 		load_balancer_sku		= "standard"
-		service_cidr			= "192.168.0.0/24"
-		dns_service_ip 			= "192.168.0.10"
-		docker_bridge_cidr 		= "172.17.0.1/16"
+		service_cidr			= "172.16.0.0/22"
+		dns_service_ip 			= "172.16.0.10"
+		docker_bridge_cidr 		= "172.16.0.1/16"
 	}
 
 	default_node_pool = {
-		name = "defaultnp"
+		name = "defaultnp01"
 		enable_auto_scaling = true
 		node_count = 2
 		min_count = 2
 		max_count = 5
-		vm_size = "Standard_D4s_v3"
+		vm_size = "Standard_D2s_v3"
 		type    = "VirtualMachineScaleSets"
 		os_disk_size_gb = 30
 		only_critical_addons_enabled = true
-	}
-
-	user_node_pools = {
-		"usernp1" = {
-			vm_size = "Standard_D4s_v3"
-			node_count = 2
-			node_labels = null
-			node_taints = []
-		}
-
-		# "rooknp01" = {
-		# 	vm_size = "Standard_D4s_v3"
-		# 	node_count = 3
-		# 	node_labels = null
-		# 	node_taints = ["storage-node=true:NoSchedule"]
-		# }
 	}
 }
